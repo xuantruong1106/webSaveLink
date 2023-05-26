b803a500547efa8ec1b56f39dcba7d8923682807
trường nguyễn xuân <91445722+xuantruong1106@users.noreply.github.com>
2023-05-26 20:38:36 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import header1 from './views/header_footer/header.vue';
+import footer1 from './views/header_footer/footer.vue';
+import data1 from './views/data.vue';
+</script>
+
+<template>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <header1/>
+  <body>
+    <data1/>
+  </body>
+  <footer1></footer1>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/data.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>data</h1>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
