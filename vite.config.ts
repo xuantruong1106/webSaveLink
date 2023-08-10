55853c24c3e01c20f8791ef591f65e90facc1a24
xuantruong1106 <truongnx.21it@vku.udn.vn>
2023-08-10 16:13:24 +0700

    run docker done

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: true,
+    port: 8000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+     watch: {
+       usePolling: true
+     }
   }
 })
